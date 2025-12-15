7dc039a1d0bee757f1dfd65ecbea59972b65862c
mohamed el-gohary <cy84z47qjh@privaterelay.appleid.com>
2025-12-16 01:44:45 +0200

    Refactor App component and update imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,26 @@
-import './App.css'
-import Contact from './Components/Contact'
-import Home from './Components/Home'
-import About from './Components/About'
-import Navbar from './Components/Navbar'
-import Footer from './Components/Footer'
-import Portfolio from './Components/Portfolio'
-import React from 'react'
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Router>
+    <>
       <Navbar />
+
       <Routes>
-        <Route path="/" element={ <Home />} />
-        <Route path="/portfolio" element={ <Portfolio />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-      <Footer/>
-    </Router>
-  )
+
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
