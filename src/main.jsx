540da3087d931c2e796ee1e511a8d45194ac819f
mohamed el-gohary <cy84z47qjh@privaterelay.appleid.com>
2025-12-16 01:47:34 +0200

    Refactor main.jsx to use StrictMode and update root

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
-import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+createRoot(document.getElementById('test')).render(
+  <StrictMode>
     <App />
-  </HashRouter>
-);
-
+  </StrictMode>
+)
