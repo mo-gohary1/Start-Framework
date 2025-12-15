06b1e68945d151aa8ca7c7289d7c6a91a477adbf
mohamed el-gohary <cy84z47qjh@privaterelay.appleid.com>
2025-12-16 01:33:18 +0200

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
     >
       <div className="text-center">
         <img
-          src="/src/assets/images/avataaars.svg"
+          src="../assets/images/avataaars."
           alt="avatar"
           className="mb-3"
           width={250}
